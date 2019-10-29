bf0e30f48c065d8bda6df29fb92d36164d6b783d
sAnjali12 <anjalisen060@gmail.com>
2019-10-29 11:23:53 +0530

    create words.py and word.txt file for the secrete word

---
+++ words.py
@@ -0,0 +1,30 @@
+import string
+import random
+
+def load_words():
+    """
+    Ye function kaafi jayada words ko load karne mai help karega
+    """
+    my_data=open("words.txt","r")
+    #Data=my_data.read()
+
+    #my_file=my_data.load()
+    my_files=my_data.readlines()
+    Type=""
+
+    for line in my_files:
+        Type = line.split(" ")
+    return Type
+# print load_words()
+
+def choose_word():
+
+    word_list = load_words()
+    secret_word = random.choice(word_list)
+    secret_word = secret_word.lower()
+
+    return secret_word
+print choose_word()
+#  list1 = ['1', '2', '3']
+# str1 = ''.join(list1)
+# print str1
